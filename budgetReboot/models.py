d0e06a9871802a406cc0d6a5261d0c416ac70ee7
Solomon Grundy <ngwtt@yahoo.com>
2020-05-14 20:36:19 -0600

    adding mild design artifact + initial database work

--- budgetReboot/models.py
+++ budgetReboot/models.py
@@ -4,31 +4,64 @@ from django.utils import timezone
 
 #                        user
          #                 /    |     \
-       # time            jan   feb  dec    etc.
+       # categories bank |food  |entertainment| personal care
        #                / |                |                                   \
-         # categories bank |food  |entertainment| personal care
+         # period    jan   feb  dec     (...+year)   etc.
      #                             /       |             \
     # entries                     entry1  entry2    entry 1   etc. 
 
 #  user1, jan 2020: banking, groceries - user1, feb 2020: banking groceries ...user12 nov 2021: groceries - user 12 dec 2021: groceries...
 
 class User(models.Model):
-    users_name = models.CharField(max_length=200)
+    users_name = models.CharField(max_length=200)    
+
+
+class Category(models.Model):
+    owning_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=200)    
     
+    overall_total = models.DecimalField(default=0, max_digits=11, decimal_places=2)
+    total_entries = models.IntegerField(default=0)
+
+
     
-class Period(models.Model):
-    numeric_month = models.IntegerField(default=0)
-    numeric_year = models.IntegerField(default=0)
+class CatPeriod(models.Model):
+    numeric_month = models.IntegerField(default=timezone.now().month)
+    numeric_year = models.IntegerField(default=timezone.now().year)
     
-    character_month = models.CharField(default='unknown', max-length = 11)
-    character_year = models.CharField(default= 'unknown', max-length = 5)
+    def is_current_month(self):
+        this_moment = timezone.now().month
+        return numeric_month ==  transaction_date.month
+    
+    cat_month = models.CharField(max_length = 11)
+    cat_year = models.CharField(max_length = 5)
+    
+    monthly_total = models.DecimalField(default=0, max_digits=11, decimal_places=2)    
+    monthly_entry_count = models.IntegerField(default=0)
     
     cat = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     
-class Category(models.Model):
-    owningUser = models.ForeignKey(User, on_delete=models.CASCADE)
-    category_name = models.CharField(max_length=200)
-    overall_total = models.DecimalField(default=0, max_digits=11, decimal_places=2)
-    monthly_total = models.DecimalField(default=0, max_digits=11, decimal_places=2)    
-    total_entries = models.IntegerField(default=0)
+    
+    
+class Entry(models.Model):
+    cat = models.ForeignKey(CatPeriod, on_delete=models.CASCADE)
+    entry_note=models.CharField(default='',max_length=11)
+    amount = models.DecimalField(default=0, max_digits=11, decimal_places=2)
+    transaction_date=models.DateTimeField('transaction date',default=timezone.now, blank=True)
+    
+    def is_current_month(self):
+        this_moment = timezone.now().month
+        return this_moment ==  transaction_date.month
+    
+    def __str__(self):
+        return self.entry_note
+
+class AggregateStats(models.Model):
+    owning_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    grand_total = models.DecimalField(default=0, max_digits=9, decimal_places=2)
+    avg_monthly_expeditures = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    grand_income = models.DecimalField(default=0, max_digits=9, decimal_places=2)
+    avg_monthly_income  = models.DecimalField(default=0, max_digits=9, decimal_places=2)
+    number_of_months = models.IntegerField(default=0)
+    current_month_total = models.DecimalField(default=0, max_digits=8, decimal_places=2)
